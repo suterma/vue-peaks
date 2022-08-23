44e1864b3a806dc7f9fce7e43be325a940ec8d53
suterma <git@marcelsuter.ch>
2022-08-23 23:21:11 +0200

    logo and info update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string
-}>()
+  msg: string;
+}>();
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+      A Vue wrapper around the bbc/peaks.js audio waveform UI component.
     </h3>
   </div>
 </template>
@@ -32,6 +30,7 @@ h3 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
