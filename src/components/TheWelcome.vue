07cce45fa97df99e5042c38398121fa5d5d3627e
Marcel Suter <github@marcelsuter.ch>
2022-08-24 06:27:01 +0000

    removed unused icons

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
 </script>
 
 <template>
@@ -15,15 +12,18 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #heading>Documentation</template>
     <p>
       Explore the below examples or see the
-      <a target="_blank" href="https://github.com/suterma/vue-peaks#vue-peaks">vue-peaks documentation on GitHub</a>.
+      <a target="_blank" href="https://github.com/suterma/vue-peaks#vue-peaks"
+        >vue-peaks documentation on GitHub</a
+      >.
     </p>
 
     <p>
       See the
-      <a target="_blank" href="https://waveform.prototyping.bbc.co.uk/">peaks.js documentation</a>
+      <a target="_blank" href="https://waveform.prototyping.bbc.co.uk/"
+        >peaks.js documentation</a
+      >
       for detailed information about the options and tools.
     </p>
-
   </WelcomeItem>
 
   <WelcomeItem>
@@ -34,5 +34,4 @@ import SupportIcon from './icons/IconSupport.vue'
 
     See the below examples to get an idea how to use the wrapper components.
   </WelcomeItem>
-
 </template>
