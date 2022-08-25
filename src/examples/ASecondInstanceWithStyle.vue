e7dbe313cf533bd60d13fa23384a38e772177d1f
Marcel Suter <github@marcelsuter.ch>
2022-08-25 14:46:42 +0000

    better styles for structure

--- src/examples/ASecondInstanceWithStyle.vue
+++ src/examples/ASecondInstanceWithStyle.vue
@@ -6,18 +6,33 @@ import AudioPeaks from './../components/AudioPeaks.vue';
   <h1>A second instance with class styles</h1>
   <pre>
 &lt;AudioPeaks src="lidija_roos-decisions.ogg" id="someUniqueIdentifier" /&gt;
+...
+&lt;style&gt;
+.peaks-overview:hover,
+.peaks-zoomview:hover {
+  background-color: hsla(160, 100%, 37%, 0.2);
+}
+
+.peaks-overview,
+.peaks-zoomview {
+  height: 50px;
+}
+&lt;/style&gt;
   </pre>
   <p>
     When using more than one component (more than one Peaks.js) instance in the
-    same HTML document, a unique id must be set on each instance. This id is
+    same HTML document, a <b>unique id</b> must be set on each instance. This id is
     applied to the panes and the the audio element, respectively, with a prefix.
   </p>
   <p>
-    Each pane (overview, zoomview, audio, controls) has dedicated CSS classes which you can use to customize the look
+    Each pane (overview, zoomview, audio, controls) has <b>dedicated CSS classes</b> which you can use to customize the
+    look
     and feel. Note however, that you can not style the waveforms themselves this way.
   </p>
-  <div id="audiopeaks-css">
-    <AudioPeaks src="lidija_roos-decisions.ogg" id="someUniqueIdentifier" />
+  <div class="example-display">
+    <div id="audiopeaks-css">
+      <AudioPeaks src="lidija_roos-decisions.ogg" id="someUniqueIdentifier" />
+    </div>
   </div>
 </template>
 
