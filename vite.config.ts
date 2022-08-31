9f578060470cb06f323c3234a291b5ce9c17b0cb
Marcel Suter <github@marcelsuter.ch>
2022-08-31 08:19:44 +0000

    externalized audio files

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { url } from 'node:inspector';
 
 // https://vitejs.dev/config/
 export default defineConfig({
