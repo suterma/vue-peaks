0466127c7c6ee4b6207fbd13305617c364222622
suterma <git@marcelsuter.ch>
2022-09-08 00:13:34 +0200

    updated the demo widget

--- src/components/AudioPeaks.vue
+++ src/components/AudioPeaks.vue
@@ -1,64 +1,64 @@
 <script setup lang="ts">
 import {
-    ref,
-    shallowRef,
-    onMounted,
-    type ShallowRef,
-    onBeforeUnmount,
+  ref,
+  shallowRef,
+  onMounted,
+  type ShallowRef,
+  onBeforeUnmount,
 } from 'vue';
 import Peaks, { type PeaksOptions } from 'peaks.js';
 
 const props = defineProps<{
-    /** The audio source URL (for the "simple" mode)
-     * @remarks This URL is to be used internally with the default media slot
-     * (no slot template or external media element id is expected)
-     */
-    src?: string;
+  /** The audio source URL (for the "simple" mode)
+   * @remarks This URL is to be used internally with the default media slot
+   * (no slot template or external media element id is expected)
+   */
+  src?: string;
 
-    /** The unique identifier of an external zoomview element to use.
-     * @remarks Allows the use of an external zoomview element.
-     * (no slot template is expected or used for the zoomview)
-     */
-    zoomviewElementId?: string;
+  /** The unique identifier of an external zoomview element to use.
+   * @remarks Allows the use of an external zoomview element.
+   * (no slot template is expected or used for the zoomview)
+   */
+  zoomviewElementId?: string;
 
-    /** The zoomview element to use.
-     * @remarks Allows the use of an external zoomview element.
-     * (no slot template is expected or used for the zoomview)
-     */
-    zoomviewElement?: HTMLDivElement;
+  /** The zoomview element to use.
+   * @remarks Allows the use of an external zoomview element.
+   * (no slot template is expected or used for the zoomview)
+   */
+  zoomviewElement?: HTMLDivElement;
 
-    /** The unique identifier of an external overview element to use.
-     * @remarks Allows the use of an external overview element.
-     * (no slot template is expected or used for the overview)
-     */
-    overviewElementId?: string;
+  /** The unique identifier of an external overview element to use.
+   * @remarks Allows the use of an external overview element.
+   * (no slot template is expected or used for the overview)
+   */
+  overviewElementId?: string;
 
-    /** The overview element to use.
-     * @remarks Allows the use of an external overview element.
-     * (no slot template is expected or used for the overview)
-     */
-    overviewElement?: HTMLDivElement;
+  /** The overview element to use.
+   * @remarks Allows the use of an external overview element.
+   * (no slot template is expected or used for the overview)
+   */
+  overviewElement?: HTMLDivElement;
 
-    /** The unique identifier of an external media element to use. (for the "external" mode)
-     * @remarks Allows the use of an external media element.
-     * (no slot template or audio source URL is expected)
-     */
-    mediaElementId?: string;
+  /** The unique identifier of an external media element to use. (for the "external" mode)
+   * @remarks Allows the use of an external media element.
+   * (no slot template or audio source URL is expected)
+   */
+  mediaElementId?: string;
 
-    /** The external media element to use. (for the "external" mode)
-     * @remarks Allows the use of an external media element.
-     * (no slot template or audio source URL is expected)
-     */
-    mediaElement?: HTMLMediaElement;
+  /** The external media element to use. (for the "external" mode)
+   * @remarks Allows the use of an external media element.
+   * (no slot template or audio source URL is expected)
+   */
+  mediaElement?: HTMLMediaElement;
 
-    /** The peaks options to use.
-     * @remarks The element references to the overview, zoomview and media element
-     * should not be provided, because these are handled internally by AudioPeaks.
-     * This removes the burden of management of the Vue ref lifecycle from the user.
-     * @devdoc Refs to HTML elements can only be accessed after mount.
-     * See https://vuejs.org/guide/essentials/template-refs.html#accessing-the-refs
-     */
-    options?: Peaks.PeaksOptions;
+  /** The peaks options to use.
+   * @remarks The element references to the overview, zoomview and media element
+   * should not be provided, because these are handled internally by AudioPeaks.
+   * This removes the burden of management of the Vue ref lifecycle from the user.
+   * @devdoc Refs to HTML elements can only be accessed after mount.
+   * See https://vuejs.org/guide/essentials/template-refs.html#accessing-the-refs
+   */
+  options?: Peaks.PeaksOptions;
 }>();
 
 /** The peaks instance MUST NOT be deeply reactive for performance reasons.
@@ -74,12 +74,21 @@ const audio = shallowRef(null);
 const audioSlot = shallowRef(null);
 const zoomLevel = shallowRef<number | undefined>(undefined);
 
+
+defineExpose({
+  /** The peaks.js instance is deliberately exposed, to allow direct use of the various APIs.
+   * @remarks The instance is only available after the mounted lifecycle event and once
+   * peaks.js has properly initialized. 
+   */
+  peaksInstance
+})
+
 onMounted(() => {
-    createPeaksInstance();
+  createPeaksInstance();
 });
 
 onBeforeUnmount(() => {
-    destroyPeaksInstance();
+  destroyPeaksInstance();
 });
 
 /** Initializes the peaks instance
@@ -92,65 +101,65 @@ onBeforeUnmount(() => {
  * (audio.value as unknown as HTMLAudioElement)
  */
 function createPeaksInstance() {
-    console.debug('AudioPeaks::createPeaksInstance:options:', props.options);
+  console.debug('AudioPeaks::createPeaksInstance:options:', props.options);
 
-    const overviewElement = get<HTMLDivElement>(
-        props.overviewElement,
-        props.overviewElementId,
-        overview,
-        overviewSlot,
-        'div'
-    );
-    const zoomviewElement = get<HTMLDivElement>(
-        props.zoomviewElement,
-        props.zoomviewElementId,
-        zoomview,
-        zoomviewSlot,
-        'div'
-    );
-    const mediaElement = get<HTMLMediaElement>(
-        props.mediaElement,
-        props.mediaElementId,
-        audio,
-        audioSlot,
-        'audio'
-    );
+  const overviewElement = get<HTMLDivElement>(
+    props.overviewElement,
+    props.overviewElementId,
+    overview,
+    overviewSlot,
+    'div'
+  );
+  const zoomviewElement = get<HTMLDivElement>(
+    props.zoomviewElement,
+    props.zoomviewElementId,
+    zoomview,
+    zoomviewSlot,
+    'div'
+  );
+  const mediaElement = get<HTMLMediaElement>(
+    props.mediaElement,
+    props.mediaElementId,
+    audio,
+    audioSlot,
+    'audio'
+  );
 
-    if (props.options) {
-        (props.options.containers = {
-            overview: overviewElement,
-            zoomview: zoomviewElement,
-        }),
-            (props.options.mediaElement = mediaElement);
+  if (props.options) {
+    (props.options.containers = {
+      overview: overviewElement,
+      zoomview: zoomviewElement,
+    }),
+      (props.options.mediaElement = mediaElement);
+  }
+
+  const options: PeaksOptions = props.options
+    ? props.options
+    : {
+      containers: {
+        overview: overviewElement,
+        zoomview: zoomviewElement,
+      },
+      mediaElement: mediaElement,
+      webAudio: {
+        audioContext: new AudioContext(),
+      },
+      zoomLevels: [256, 512, 1024, 2048, 4096],
+    };
+
+  Peaks.init(options, function (err, peaks) {
+    if (err) {
+      console.error(err);
     }
-
-    const options: PeaksOptions = props.options
-        ? props.options
-        : {
-            containers: {
-                overview: overviewElement,
-                zoomview: zoomviewElement,
-            },
-            mediaElement: mediaElement,
-            webAudio: {
-                audioContext: new AudioContext(),
-            },
-            zoomLevels: [256, 512, 1024, 2048, 4096],
-        };
-
-    Peaks.init(options, function (err, peaks) {
-        if (err) {
-            console.error(err);
-        }
-        peaksInstance.value = peaks;
-        zoomLevel.value = peaks?.zoom.getZoom();
-    });
+    peaksInstance.value = peaks;
+    zoomLevel.value = peaks?.zoom.getZoom();
+  });
 }
 
 /** Destroys the peaks instance
  */
 function destroyPeaksInstance() {
-    peaksInstance.value?.destroy();
+  peaksInstance.value?.destroy();
 }
 
 /** Gets the HTML element to act upon, using the first of the provided options
@@ -168,98 +177,98 @@ function destroyPeaksInstance() {
  * @return The found element or undefined if none is found
  */
 function get<HEType extends HTMLElement>(
-    elm: HEType | undefined,
-    htmlElementId: string | undefined,
-    ref: ShallowRef,
-    slotRef: ShallowRef,
-    slotRefHtmlTagName: string
+  elm: HEType | undefined,
+  htmlElementId: string | undefined,
+  ref: ShallowRef,
+  slotRef: ShallowRef,
+  slotRefHtmlTagName: string
 ): HEType | undefined {
-    if (elm) {
-        console.debug('AudioPeaks::Found elm: ', elm);
-        return elm;
-    }
-    if (htmlElementId) {
-        console.debug('AudioPeaks::Found element by id: ', htmlElementId);
-        return document.getElementById('' + htmlElementId) as unknown as HEType;
-    }
+  if (elm) {
+    console.debug('AudioPeaks::Found elm: ', elm);
+    return elm;
+  }
+  if (htmlElementId) {
+    console.debug('AudioPeaks::Found element by id: ', htmlElementId);
+    return document.getElementById('' + htmlElementId) as unknown as HEType;
+  }
 
-    // The reference can be used only with the default slot
-    const elementByRef = ref.value as unknown as HEType;
-    if (elementByRef) {
-        console.debug('AudioPeaks::Found element by Ref: ', elementByRef);
-        return elementByRef;
-    }
+  // The reference can be used only with the default slot
+  const elementByRef = ref.value as unknown as HEType;
+  if (elementByRef) {
+    console.debug('AudioPeaks::Found element by Ref: ', elementByRef);
+    return elementByRef;
+  }
 
-    // With external slot templates, the element can not be referenced with a ref directly
-    // (because external slot templates can not set refs to enclosed elements from the outside)
-    // First get a reference to the slot then find the first element of the given type
-    const externalSlot = slotRef.value as unknown as HTMLElement;
-    if (externalSlot) {
-        const elementByFirstInSlot = externalSlot.getElementsByTagName(
-            slotRefHtmlTagName
-        )[0] as unknown as HEType;
-        if (elementByFirstInSlot) {
-            console.debug(
-                'AudioPeaks::Found element by first in slot: ',
-                elementByFirstInSlot
-            );
-            return elementByFirstInSlot;
-        }
+  // With external slot templates, the element can not be referenced with a ref directly
+  // (because external slot templates can not set refs to enclosed elements from the outside)
+  // First get a reference to the slot then find the first element of the given type
+  const externalSlot = slotRef.value as unknown as HTMLElement;
+  if (externalSlot) {
+    const elementByFirstInSlot = externalSlot.getElementsByTagName(
+      slotRefHtmlTagName
+    )[0] as unknown as HEType;
+    if (elementByFirstInSlot) {
+      console.debug(
+        'AudioPeaks::Found element by first in slot: ',
+        elementByFirstInSlot
+      );
+      return elementByFirstInSlot;
     }
+  }
 }
 
 function zoomIn(): void {
-    peaksInstance.value?.zoom.zoomIn();
-    zoomLevel.value = peaksInstance.value?.zoom.getZoom();
+  peaksInstance.value?.zoom.zoomIn();
+  zoomLevel.value = peaksInstance.value?.zoom.getZoom();
 }
 function zoomOut(): void {
-    peaksInstance.value?.zoom.zoomOut();
-    zoomLevel.value = peaksInstance.value?.zoom.getZoom();
+  peaksInstance.value?.zoom.zoomOut();
+  zoomLevel.value = peaksInstance.value?.zoom.getZoom();
 }
 </script>
 
 <template>
-    <div ref="overviewSlot">
-        <!-- If an external overview element is referenced, the overview slot is not used -->
-        <slot name="overview" v-if="!props.overviewElementId && !props.overviewElement">
-            <div class="peaks-overview" ref="overview"></div>
-        </slot>
-    </div>
-
-    <div ref="zoomviewSlot">
-        <!-- If an external zoomview element is referenced, the zoomview slot is not used -->
-        <slot name="zoomview" v-if="!props.zoomviewElementId && !props.zoomviewElement">
-            <div class="peaks-zoomview" ref="zoomview"></div>
-        </slot>
-    </div>
-
-    <div ref="audioSlot">
-        <!-- If an external media element is referenced, the default slot is not used -->
-        <slot name="default" v-if="!props.mediaElementId && !props.mediaElement">
-            <!-- The default content slot for the "slot" mode -->
-            <audio class="peaks-audio" ref="audio" controls>
-                <source :src="src" />
-            </audio>
-        </slot>
-    </div>
-    <slot name="controls">
-        <div class="peaks-controls">
-            <button @click="zoomIn()">Zoom in</button>&nbsp;
-            <button @click="zoomOut()">Zoom out</button>&nbsp;
-            <span>Zoom level: {{ zoomLevel }}</span>
-        </div>
+  <div ref="overviewSlot">
+    <!-- If an external overview element is referenced, the overview slot is not used -->
+    <slot name="overview" v-if="!props.overviewElementId && !props.overviewElement">
+      <div class="peaks-overview" ref="overview"></div>
     </slot>
+  </div>
+
+  <div ref="zoomviewSlot">
+    <!-- If an external zoomview element is referenced, the zoomview slot is not used -->
+    <slot name="zoomview" v-if="!props.zoomviewElementId && !props.zoomviewElement">
+      <div class="peaks-zoomview" ref="zoomview"></div>
+    </slot>
+  </div>
+
+  <div ref="audioSlot">
+    <!-- If an external media element is referenced, the default slot is not used -->
+    <slot name="default" v-if="!props.mediaElementId && !props.mediaElement">
+      <!-- The default content slot for the "slot" mode -->
+      <audio class="peaks-audio" ref="audio" controls>
+        <source :src="src" />
+      </audio>
+    </slot>
+  </div>
+  <slot name="controls">
+    <div class="peaks-controls">
+      <button @click="zoomIn()">Zoom in</button>&nbsp;
+      <button @click="zoomOut()">Zoom out</button>&nbsp;
+      <span>Zoom level: {{ zoomLevel }}</span>
+    </div>
+  </slot>
 </template>
 
 <style scoped>
 .peaks-audio,
 .peaks-overview,
 .peaks-zoomview {
-    width: 100%;
+  width: 100%;
 }
 
 .peaks-overview,
 .peaks-zoomview {
-    height: 100px;
+  height: 100px;
 }
 </style>
--- src/examples/DemoWidget.vue
+++ src/examples/DemoWidget.vue
@@ -1,56 +1,142 @@
 <script setup lang="ts">
-import { computed, reactive, ref, shallowRef } from 'vue';
+import type { PeaksOptions } from 'peaks.js';
+import { reactive, shallowRef } from 'vue';
 import AudioPeaks from './../components/AudioPeaks.vue';
+
+/** The configuration options 
+* @remarks The colors are taken from the Bulma color scheme.
+*/
+const options: PeaksOptions = {
+  overview: {
+    /* container is provided and handled internally by AudioPeaks */
+    waveformColor: 'hsl(141, 53%, 53%)',
+    playedWaveformColor: 'hsl(141, 53%, 31%)',
+    showAxisLabels: false,
+    axisGridlineColor: 'hsl(0, 0%, 100%)',
+  },
+
+  /* mediaElement is provided and handled internally by AudioPeaks */
+  webAudio: { audioContext: new AudioContext() },
+  zoomLevels: [0],
+  playheadColor: 'hsl(348, 100%, 61%)'
+}
 
 const audio = shallowRef<HTMLAudioElement | null>(null)
 
-// a computed ref
-const isPlaying = computed(() => {
-  return audio?.value?.duration > 0 ? true : false;
-})
+//TODO put that in a separate article
+// Accessing the instance  
+//const audioPeaksWidget = ref<InstanceType<typeof AudioPeaks>>()
+//audioPeaksWidget.value?.peaksInstance?.player.pause();
 
-//const state = reactive({ paused: ($refs.slotAudio as HTMLAudioElement).paused });
+const state = reactive({ isPlaying: false, canPlay: false });
+
+function togglePlayback() {
+  state.isPlaying = !state.isPlaying;
+  if (state.isPlaying) {
+    audio.value?.play();
+  }
+  else {
+    audio.value?.pause();
+  }
+}
 </script>
-
 <template>
-  <h1>A simple waveform widget</h1>
+  <h1>A simple waveform player widget</h1>
   <p>
     A use case for a snippets player</p>
   <pre>
 //TODO</pre>
-
-
   <div class="example-display">
 
-    {{ isPlaying ? 'Play' : 'Pause' }}
+    <div class="widget">
+      <!-- {{ state.isPlaying ? 'Play' : 'Pause' }} -->
+      <button class="item" @click="togglePlayback()" :disabled="!state.canPlay">
 
+        <!-- pause icon -->
+        <svg v-if="state.isPlaying" style="width:24px;height:24px" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M14,19H18V5H14M6,19H10V5H6V19Z" />
+        </svg>
+        <!-- play icon -->
+        <svg v-else style="width:24px;height:24px" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+        </svg>
+      </button>
+      <AudioPeaks overviewElementId="widgetOverview" ref="audioPeaksWidget" :options="options">
+        <template #zoomview>
+          <template />
+        </template>
+        <template #default>
+          <audio ref="audio" @canplay="()=>{state.canPlay = true}">
+            <source src="https://suterma.github.io/vue-peaks/lidija_roos-not_for_sale.mp3" />
+          </audio>
+        </template>
+        <template #controls>
+          <template />
+        </template>
+      </AudioPeaks>
+      <div class="item peaks-overview" id="widgetOverview">
+        <!-- spinner icon -->
+        <svg class="spin" style="width:24px;height:24px" viewBox="0 0 24 24">
+          <path fill="currentColor"
+            d="M12 19C13.1 19 14 19.9 14 21S13.1 23 12 23 10 22.1 10 21 10.9 19 12 19M12 1C13.1 1 14 1.9 14 3S13.1 5 12 5 10 4.1 10 3 10.9 1 12 1M6 16C7.1 16 8 16.9 8 18S7.1 20 6 20 4 19.1 4 18 4.9 16 6 16M3 10C4.1 10 5 10.9 5 12S4.1 14 3 14 1 13.1 1 12 1.9 10 3 10M6 4C7.1 4 8 4.9 8 6S7.1 8 6 8 4 7.1 4 6 4.9 4 6 4M18 16C19.1 16 20 16.9 20 18S19.1 20 18 20 16 19.1 16 18 16.9 16 18 16M21 10C22.1 10 23 10.9 23 12S22.1 14 21 14 19 13.1 19 12 19.9 10 21 10M18 4C19.1 4 20 4.9 20 6S19.1 8 18 8 16 7.1 16 6 16.9 4 18 4Z" />
+        </svg>
+      </div>
 
-    <button @click="audio?.play()">
-      Play
-    </button>&nbsp;
-    <button @click="audio?.pause()">
-      Pause
-    </button>
-    <div id="overviewWidget">
     </div>
-    <AudioPeaks src="https://suterma.github.io/vue-peaks/lidija_roos-decisions.ogg" overviewElementId="overviewWidget">
-      <template #zoomview>
-        <template />
-      </template>
-      <template #default>
-        <audio ref="audio">
-          <source src="https://suterma.github.io/vue-peaks/lidija_roos-not_for_sale.mp3" />
-        </audio>
-      </template>
-      <template #controls>
-        <template />
-      </template>
-    </AudioPeaks>
   </div>
 </template>
 <style scoped>
-button,
-#overviewWidget {
+.widget {
   height: 48px;
+  align-items: center;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+}
+
+.item {
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid var(--color-border);
+  background: var(--color-background);
+  border-radius: 8px;
+  cursor: pointer;
+  padding-left: 1em;
+  padding-right: 1em;
+  text-align: center;
+}
+
+/** The overview item should use all available space, including in the item itself */
+.item.peaks-overview {
+  flex-grow: 1;
+  padding-left: 0;
+  padding-right: 0;
+}
+
+.widget .item:not(:last-child) {
+  border-bottom-right-radius: 0;
+  border-top-right-radius: 0;
+  margin-right: -1px;
+}
+
+.widget .item:not(:first-child) {
+  border-bottom-left-radius: 0;
+  border-top-left-radius: 0;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(359deg);
+  }
+}
+
+.spin {
+  animation: spin 6s linear infinite;
 }
 </style>
