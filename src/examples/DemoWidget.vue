39c28adc37f68ff6f61c22c407371abc29b38cbb
suterma <git@marcelsuter.ch>
2022-09-09 22:09:20 +0200

    updated widget demo

--- src/examples/DemoWidget.vue
+++ src/examples/DemoWidget.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
 import AudioPeaksWidget from '../components/AudioPeaksWidget.vue';
+
+const state = reactive({ url: '' });
 
 </script>
 <template>
@@ -14,9 +17,30 @@ import AudioPeaksWidget from '../components/AudioPeaksWidget.vue';
     &lt;AudioPeaksWidget src="https://suterma.github.io/vue-peaks/lidija_roos-not_for_sale.mp3"&gt;&lt;/AudioPeaksWidget&gt;
     &lt;/div&gt;
   </code></pre>
+
   <div class="example-display">
-    <AudioPeaksWidget style='width:50%' src="https://suterma.github.io/vue-peaks/lidija_roos-not_for_sale.mp3">
+    <input type="url" v-model="state.url" placeholder="Paste an URL to a media file" />
+    <AudioPeaksWidget :src="state.url" :key="state.url">
     </AudioPeaksWidget>
   </div>
 </template>
- 
+<style scoped>
+input {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid var(--color-border);
+  background: var(--color-background);
+  border-radius: 8px;
+  padding-left: 1em;
+  padding-right: 1em;
+  text-align: center;
+  line-height: 1.6;
+  font-size: 15px;
+}
+
+input {
+  margin-bottom: 1em;
+  width: 100%;
+}
+</style>
