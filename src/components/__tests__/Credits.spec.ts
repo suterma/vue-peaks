dbd265b800267a3b8b2862dbfcd2e2486ef4cbba
suterma <git@marcelsuter.ch>
2022-09-15 22:23:28 +0200

    updated example test

--- src/components/__tests__/Credits.spec.ts
+++ src/components/__tests__/Credits.spec.ts
@@ -3,9 +3,9 @@ import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import Credits from '../../documentation/Credits.vue';
 
-describe('HelloWorld', () => {
+describe('Credits', () => {
   it('renders properly', () => {
     const wrapper = mount(Credits, {});
-    expect(wrapper.text()).toContain('Lidija Roos');
+    expect(wrapper.html()).toContain('Lidija Roos');
   });
 });
