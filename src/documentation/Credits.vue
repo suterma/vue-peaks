258332e865dbf20a58e5742c79de7de1107f38db
Marcel Suter <github@marcelsuter.ch>
2022-09-15 10:00:16 +0000

    updated credits

--- src/documentation/Credits.vue
+++ src/documentation/Credits.vue
@@ -3,14 +3,14 @@
   <p>This project uses</p>
   <ul>
     <li>
+      <a href="https://github.com/bbc/peaks.js/" target="_blank">peaks.js</a>,
+      a client-side JavaScript component to display and interact with audio waveforms in the browser. (Released under
+      the LGPL-3.0 license, by the British Broadcasting Corporation and contributors)
+    </li>
+    <li>
       <a href="https://vuejs.org/" target="_blank">Vue.js</a>, the
       progressive JavaScript framework. (Released under the MIT
       License, Copyright © 2014-2021 Evan You)
-    </li>
-    <li>
-      <a href="https://vuex.vuejs.org/" target="_blank">peaks.js</a>,
-      a client-side JavaScript component to display and interact with audio waveforms in the browser. (Released under
-      the LGPL-3.0 license, by the British Broadcasting Corporation and contributors)
     </li>
     <li> <a href="https://bulma.io/" target="_blank">Bulma</a>, the
       modern CSS framework that just works. (Released under the
@@ -34,6 +34,46 @@
     </li>
     <li>
       Music from the Album "Not For Sale" by Lidija Roos (Released to the public, by Lidija Roos)
+    </li>
+
+    <li><a href="https://github.com/highlightjs/vue-plugin" target="_blank">@highlightjs/vue-plugin</a>, provides a
+      highlightjs component for use in your Vue.js 3 applications. (Released under the BSD-3-Clause license, by the
+      highlight.js team) </li>
+    <li><a href="https://github.com/microsoft/rushstack/tree/main/eslint/eslint-patch"
+        target="_blank">@rushstack/eslint-patch</a>, a patch that improves how ESLint loads plugins when working in a
+      monorepo with a reusable toolchain. (Released under the
+      MIT License, by Microsoft)</li>
+    <li><a href="https://github.com/DefinitelyTyped/DefinitelyTyped" target="_blank">@types/jsdom, @types/node</a>, the
+      repository
+      for high quality TypeScript type definitions. (Released under the
+      MIT License, by various contributors)</li>
+    <li><a href="https://github.com/vitejs/vite" target="_blank">vite, vitest, @vitejs/plugin-vue</a>, next Generation
+      Frontend Tooling. (Released under the
+      MIT License, by the vite team)
+    </li>
+    <li><a href="https://github.com/eslint/eslint" target="_blank">eslint</a>, a tool for identifying and reporting on
+      patterns found in ECMAScript/JavaScript code. (Released under the
+      MIT License, by the eslint team)
+    </li>
+    <li><a href="https://github.com/jsdom/jsdom" target="_blank">jsdom</a>, a pure-JavaScript implementation of many web
+      standards. (Released under the
+      MIT License, by the jsdom team)
+    </li>
+    <li><a href="https://github.com/mysticatea/npm-run-all" target="_blank">npm-run-all</a>, a CLI tool to run multiple
+      npm-scripts in parallel or sequential. (Released under the
+      MIT License, by Toru Nagashima)
+    </li>
+    <li><a href="https://github.com/prettier/prettier" target="_blank">prettier</a>, an opinionated Code Formatter.
+      (Released under the
+      MIT License, by the prettier team)
+    </li>
+    <li><a href="https://github.com/Microsoft/TypeScript" target="_blank">typescript</a>, a language for
+      application-scale JavaScript. (Released under the
+      Apache-2.0 license, by Microsoft)
+    </li>
+    <li><a href="https://github.com/johnsoncodehk/volar" target="_blank">vue-tsc</a>, high-performance tooling for Vue.
+      (Released under the
+      MIT License, by Johnson Chu)
     </li>
   </ul>
 </template>
