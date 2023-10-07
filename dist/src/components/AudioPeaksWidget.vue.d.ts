53e33abea8552e6eced930d9644f24116e8bc785
Marcel Suter <github@marcelsuter.ch>
2023-10-07 20:58:16 +0200

    built for gh-pages

---
+++ dist/src/components/AudioPeaksWidget.vue.d.ts
@@ -0,0 +1,10 @@
+declare const _default: import("vue").DefineComponent<{
+    src: {
+        type: import("vue").PropType<string>;
+    };
+}, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    src: {
+        type: import("vue").PropType<string>;
+    };
+}>>, {}, {}>;
+export default _default;
